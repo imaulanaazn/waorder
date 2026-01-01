a3c350c1e922c2a67fb369ff0162de96ef9bc7be
irham <i.maulana.azn@gmail.com>
2026-01-01 17:28:45 +0700

    change the modal style for responsive purpose

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,16 +1,16 @@
 @if ($showModal)
-<div class="fixed z-10 inset-0 overflow-y-auto">
+<div class="fixed z-50 inset-0 overflow-y-auto">
     <div class="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
         <div class="fixed inset-0 transition-opacity">
-            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+            <div class="absolute inset-0 bg-black/40"></div>
         </div>
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
         <div
-            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-            <div class="p-6 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
+            class="w-full inline-block align-bottom bg-white rounded-lg md:rounded-2xl! px-4 pt-5 pb-4 md:px-6! text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full lg:max-w-2xl! sm:p-6">
+            <div class="p-3 md:p-6 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
                 <h5>{{ $title }}</h5>
             </div>
-            <div class="flex-auto px-6 pt-6">
+            <div class="flex-auto">
                 {{ $slot }}
             </div>
 
